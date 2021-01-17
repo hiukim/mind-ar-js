d82787acbbf6bee33d4e438ddff3d52c61ffa7fb
HiuKim Yuen <hiukim@HiuKims-MacBook-Pro.local>
2021-01-17 19:00:57 +0800

    packages and webpack update

--- webpack.config.prod.cjs
+++ webpack.config.prod.cjs
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'development',
+  mode: 'production',
   output: {
     filename: 'mindar.prod.js',
     path: path.resolve(__dirname, 'dist'),
