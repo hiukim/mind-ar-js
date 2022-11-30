9e3279e11693ce42339ec8b33629818ec3af3500
jmschrack <jmschrack@gmail.com>
2022-11-29 18:47:29 -0600

    Converted computeExtremaFreak to use FakeShader

--- src/image-target/detector/ops/cpu/computeExtremaFreak.js
+++ src/image-target/detector/ops/cpu/computeExtremaFreak.js
@@ -1,55 +1,46 @@
 const { FREAKPOINTS } = require('../../freak.js')
-
-
+const FakeShader = require('./fakeShader.js')
 const FREAK_EXPANSION_FACTOR = 7.0;
-/**
- * 
- * @param {Array<*>} gaussianImagesT 
- * @param {*} prunedExtremas 
- * @param {*} prunedExtremasAngles 
- * @param {*} freakPointsT 
- * @returns 
- */
-function computeExtremaFreakImpl(gaussianImagesT, prunedExtremas, prunedExtremasAngles, freakPointsT) {
-    const resultValues = new Float32Array(prunedExtremas.height * FREAKPOINTS.length);
-    gaussianImagesT.unshift(null);
-    function getPixel(octave, y, x) {
-        const temp = gaussianImagesT[octave];
-        return temp.values[y * temp.width + x];
-    }
-    function getExtrema(featureIndex, num) {
-        return prunedExtremas.values[featureIndex * prunedExtremas.width + num];
-    }
-    function getAngles(featureIndex) {
-        return prunedExtremasAngles.values[featureIndex];
-    }
-    function getFreakPoints(freakIndex, num) {
-        return freakPointsT.values[freakIndex * freakPointsT.width + num];
+
+function GetProgram(prunedExtremasHeight, pyramidImagesLength) {
+
+    const imageVariableNames = [];
+    for (let i = 1; i < pyramidImagesLength; i++) {
+        imageVariableNames.push('image' + i);
     }
 
-    function setOutput(y, x, o) {
-        resultValues[y * FREAKPOINTS.length + x] = o;
-    }
 
-    for (let featureIndex = 0; featureIndex < prunedExtremas.height; featureIndex++) {
-        for (let freakIndex = 0; freakIndex < FREAKPOINTS.length; freakIndex++) {
-            //const freakSigma = getFreakPoints(freakIndex, 0);
-            const freakX = getFreakPoints(freakIndex, 1);
-            const freakY = getFreakPoints(freakIndex, 2);
+    const kernel = {
+        variableNames: [...imageVariableNames, 'extrema', 'angles', 'freakPoints'],
+        outputShape: [prunedExtremasHeight, FREAKPOINTS.length],
+        userCode: function () {
+            const getPixel=(octave, y, x)=> {
+                const key = 'getImage' + octave;
+                if (octave < 1 || octave >= pyramidImagesLength)
+                    return 0.0;
+                return this[key](y, x);
+            }
+            const coords = this.getOutputCoords();
+            const featureIndex = coords[0];
+            const freakIndex = coords[1];
 
-            const octave = Math.trunc(getExtrema(featureIndex, 1));
-            const inputY = getExtrema(featureIndex, 2);
-            const inputX = getExtrema(featureIndex, 3);
-            const inputAngle = getAngles(featureIndex);
+            //const freakSigma = this.getFreakPoints(freakIndex, 0);
+            const freakX = this.getFreakPoints(freakIndex, 1);
+            const freakY = this.getFreakPoints(freakIndex, 2);
+
+            const octave = this.int(this.getExtrema(featureIndex, 1));
+            const inputY = this.getExtrema(featureIndex, 2);
+            const inputX = this.getExtrema(featureIndex, 3);
+            const inputAngle = this.getAngles(featureIndex);
             const cos = FREAK_EXPANSION_FACTOR * Math.cos(inputAngle);
             const sin = FREAK_EXPANSION_FACTOR * Math.sin(inputAngle);
 
             const yp = inputY + freakX * sin + freakY * cos;
             const xp = inputX + freakX * cos + freakY * -sin;
 
-            const x0 = Math.trunc(Math.floor(xp));
+            const x0 = this.int(Math.floor(xp));
             const x1 = x0 + 1;
-            const y0 = Math.trunc(Math.floor(yp));
+            const y0 = this.int(Math.floor(yp));
             const y1 = y0 + 1;
 
             const f1 = getPixel(octave, y0, x0);
@@ -57,10 +48,10 @@ function computeExtremaFreakImpl(gaussianImagesT, prunedExtremas, prunedExtremas
             const f3 = getPixel(octave, y1, x0);
             const f4 = getPixel(octave, y1, x1);
 
-            /* const x1f = x1;
-            const y1f = y1;
-            const x0f = x0;
-            const y0f = y0; */
+            /* const x1f = float(x1);
+            const y1f = float(y1);
+            const x0f = float(x0);
+            const y0f = float(y0); */
 
             // ratio for interpolation between four neighbouring points
             const value = (x1 - xp) * (y1 - yp) * f1
@@ -68,26 +59,24 @@ function computeExtremaFreakImpl(gaussianImagesT, prunedExtremas, prunedExtremas
                 + (x1 - xp) * (yp - y0) * f3
                 + (xp - x0) * (yp - y0) * f4;
 
-            setOutput(featureIndex, freakIndex, value);
+            this.setOutput(value);
         }
+
     }
-    return resultValues;
+
+    return kernel;
+
 }
+
+
 
 const computeExtremaFreak = (args) => {
     /** @type {import('@tensorflow/tfjs').TensorInfo} */
-    const { gaussianImagesT, prunedExtremas, prunedExtremasAngles, freakPointsT,pyramidImagesLength } = args.inputs;
+    const { gaussianImagesT, prunedExtremas, prunedExtremasAngles, freakPointsT, pyramidImagesLength } = args.inputs;
     /** @type {MathBackendCPU} */
     const backend = args.backend;
-    
-    /** @type {Array<number[]>} */
-    const gaussianImagesTData = gaussianImagesT.map((tensorInfo) => { return { height: tensorInfo.shape[0], width: tensorInfo.shape[1], values: backend.data.get(tensorInfo.dataId).values, } });
-    const prunedExtremasData = { values: backend.data.get(prunedExtremas.dataId).values, height: prunedExtremas.shape[0], width: prunedExtremas.shape[1] };
-    const prunedExtremasAnglesData = { values: backend.data.get(prunedExtremasAngles.dataId).values };
-    const freakPointsTData = { values: backend.data.get(freakPointsT.dataId).values, height: freakPointsT.shape[0], width: freakPointsT.shape[1] };
-    
-    const resultValues = computeExtremaFreakImpl(gaussianImagesTData, prunedExtremasData, prunedExtremasAnglesData, freakPointsTData);
-    return backend.makeOutput(resultValues, [prunedExtremas.shape[0], FREAKPOINTS.length], 'float32');
+    const prog = GetProgram(prunedExtremas.shape[0], pyramidImagesLength);
+    return FakeShader.runCode(backend, prog, [...gaussianImagesT, prunedExtremas, prunedExtremasAngles, freakPointsT], 'float32');
 }
 
 
@@ -100,6 +89,5 @@ const computeExtremaFreakConfig = {//: KernelConfig
 
 module.exports = {
     computeExtremaFreakConfig,
-    computeExtremaFreak,
-    computeExtremaFreakImpl
+    computeExtremaFreak
 }
