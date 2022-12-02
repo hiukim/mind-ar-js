f224a1dacebc25931d1414079472b4d1fcbe3e19
jmschrack <jmschrack@gmail.com>
2022-12-01 18:22:43 -0600

    Added missing tensor input for webgl

--- src/image-target/detector/kernels/webgl/computeLocalization.js
+++ src/image-target/detector/kernels/webgl/computeLocalization.js
@@ -1,41 +1,5 @@
+import {tensor} from '@tensorflow/tfjs'
 
-/*
-let dogSubCodes = `float getPixel(int octave, int y, int x) {`;
-      for (let i = 1; i < dogPyramidImagesT.length; i++) {  // extrema starts from second octave
-	dogVariableNames.push('image' + i);
-	dogSubCodes += `
-	  if (octave == ${i}) {
-	    return getImage${i}(y, x);
-	  }
-
-      getImage3(x,y)
- 	`;
-
-const kernel = {
-	variableNames: [...dogVariableNames, 'extrema'],
-	outputShape: [prunedExtremasList.length, 3, 3], // 3x3 pixels around the extrema
-	userCode: `
-	  ${dogSubCodes}
-
-	  void main() {
-	    ivec3 coords = getOutputCoords();
-	    int featureIndex = coords[0];
-	    float score = getExtrema(featureIndex, 0);
-	    if (score == 0.0) {
-	      return;
-	    }
-
-	    int dy = coords[1]-1;
-	    int dx = coords[2]-1;
-	    int octave = int(getExtrema(featureIndex, 1));
-	    int y = int(getExtrema(featureIndex, 2));
-	    int x = int(getExtrema(featureIndex, 3));
-	    setOutput(getPixel(octave, y+dy, x+dx));
-	  }
-	`
-      }
-
-*/
 const cache={};
 function GetProgram(numDogPyramidImages,extremasListLength){
 	const kernelKey=`${numDogPyramidImages}|${extremasListLength}`;
@@ -85,7 +49,7 @@ export const computeLocalization=(args)=>{
 	/** @type {MathBackendWebGL} */
 	const backend = args.backend;
 	const program = GetProgram(dogPyramidImagesT.length,prunedExtremasList.length);
-	const prunedExtremasT = tf.tensor(prunedExtremasList, [prunedExtremasList.length, prunedExtremasList[0].length], 'int32');
+	const prunedExtremasT = tensor(prunedExtremasList, [prunedExtremasList.length, prunedExtremasList[0].length], 'int32');
 	return backend.runWebGLProgram(program, [...dogPyramidImagesT.slice(1), prunedExtremasT],dogPyramidImagesT[0].dtype);
 	 
 }
