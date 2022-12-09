577cebba8f81a30267273e060aaa199d34a71997
jmschrack <jmschrack@gmail.com>
2022-12-09 15:07:46 -0600

    Switched to async config to chain building aframe iife with es modules. updated examples accordingly

---
+++ vite.config.dev.js
@@ -0,0 +1,89 @@
+import {defineConfig,build} from 'vite'
+import * as fs from 'fs/promises';
+import * as path from 'path';
+
+const outDir = 'dist-dev'
+
+const moduleConfig={
+    mode: 'development',
+    assetsInclude:'**/*.html',
+    plugins:[],
+    build: {
+        outDir: outDir,
+        emptyOutDir:false,
+        sourcemap:'inline' ,
+        lib: {
+            fileName:"[name]",
+            entry:'index.js',
+            formats:['es']
+        },
+        rollupOptions:{
+            external:['three','three/examples/jsm/'],
+            input:{
+                'mindar-image': './src/image-target/index.js',
+                'mindar-image-three': './src/image-target/three.js',
+                'mindar-face': './src/face-target/index.js',
+                'mindar-face-three': './src/face-target/three.js',
+            }
+        }
+    }
+};
+const faceAframeConfig=defineConfig({
+    mode: 'development',
+    build: {
+        outDir: outDir,
+        emptyOutDir:false,
+        sourcemap:'inline' ,
+        lib: {
+            name:"MINDAR",
+            fileName:"[name]",
+            entry:'index.js',
+            formats:['iife']
+        },
+        rollupOptions:{
+            input:{
+                'mindar-face-aframe': './src/face-target/aframe.js',
+            },
+           
+        }
+    }
+})
+/** @type {import('vite').UserConfig} */
+const imageAframeConfig=defineConfig({
+    mode: 'development',
+    build: {
+        outDir: outDir,
+        emptyOutDir:false,
+        sourcemap:'inline' ,
+        lib: {
+            name:"MINDAR",
+            fileName:"[name]",
+            entry:'index.js',
+            formats:['iife'],
+
+        },
+        rollupOptions:{
+            input:{
+                'mindar-image-aframe': './src/image-target/aframe.js'
+            }
+        }
+    }
+})
+
+export default defineConfig(async ({ command, mode }) => {
+    await fs.rm(outDir,{recursive:true,force:true});
+    if (command === 'build') {
+        await build(imageAframeConfig);
+        await build(faceAframeConfig);
+        const files=await fs.readdir(outDir);
+        //rename the aframe builds
+        await Promise.all(files.map(async (filename)=>{
+            if(filename.includes(".iife.js")){
+                const newName=filename.replace(".iife.js",".js");
+                console.log(filename,"->",newName)
+                await fs.rename(path.join(outDir,filename),path.join(outDir,newName));
+            }
+        }));
+    }
+    return moduleConfig
+  })
